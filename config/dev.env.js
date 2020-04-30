12bba80e02d4dc24199bf4da362570854f58d1ec
lijun <984874085@qq.com>
2020-04-30 09:33:44 +0800

    更改后台访问端口

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 // 后端接口地址
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.47.1:8082"'
+  BASE_API: '"http://192.168.47.1:8081"'
 })
